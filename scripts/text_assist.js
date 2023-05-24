f9f72b802e7bd201e378174e27f158b3028a9714
Agnideep <agnideep22@gmail.com>
2023-05-24 16:37:15 +0530

    word meaning,vocabs added

--- scripts/text_assist.js
+++ scripts/text_assist.js
@@ -3,6 +3,9 @@
 
 let textBox_open = false; //toggle flag
 let prompt_text = "";
+let output_text = "";
+
+const dictionary_api = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 function removeTextPrompt() {
     const text_prompt = document.getElementById("readerai_text_prompt");
@@ -11,7 +14,7 @@ function removeTextPrompt() {
     }
 }
 
-function promptGen() {
+function promptGen(box_text, audio_play) {
     const boxx = document.createElement("div");
     boxx.id = "readerai_text_prompt";
     boxx.style.width = "auto";
@@ -28,29 +31,86 @@ function promptGen() {
     boxx.style.bottom = "4%";
     boxx.style.right = "70%";
     boxx.style.boxShadow = "0 2px 4px darkslategray";
-    boxx.style.cursor = "pointer";
-    boxx.textContent = prompt_text;
 
-    
+
+    boxx.textContent = box_text;
     const close_button = document.createElement("button");
     
     close_button.textContent = "X";
     close_button.style.backgroundColor = "#E5E4E2";
     close_button.style.borderRadius = "50%";
     close_button.style.borderWidth = "0px";
-    close_button.style.margin = "3px";
+    close_button.style.margin = "5px";
+    close_button.style.cursor = "pointer";
+    close_button.style.boxShadow = "rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset";
+
+
+    const audio_btn = document.createElement("button");
+    audio_btn.id = "readerai_audio";
+    audio_btn.style.fontFamily = '"JetBrains Mono", monospace';
+    audio_btn.textContent = "Pronunciation🔊";
+    audio_btn.style.backgroundColor = "#E5E4E2";
+    audio_btn.style.borderWidth = "0px";
+    audio_btn.style.margin = "5px";
+    audio_btn.style.cursor = "pointer";
+    audio_btn.style.padding = "3px";
+    audio_btn.style.boxShadow = "rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset"
+
+    const audioPlayer = document.createElement("audio");
+    audioPlayer.setAttribute("id", "audioPlayer");
+    audioPlayer.setAttribute("src", `${audio_play}`);
+    const playButton = document.getElementById("readerai_audio");
+    document.body.appendChild(audioPlayer);
+
 
     document.body.appendChild(boxx);
-    
     const readerai_div = document.getElementById("readerai_text_prompt");
-    readerai_div.appendChild(close_button)
+    readerai_div.appendChild(audio_btn);
+    readerai_div.appendChild(audioPlayer);
+    readerai_div.appendChild(close_button);
 
-    console.log("added text assist box!")
+
+    audio_btn.addEventListener("click", function() {
+
+        audioPlayer.play();
+      });
+
+
+
+    console.log("added text assist box!");
 
     close_button.addEventListener('click', function() {
         removeTextPrompt();
         textBox_open = !textBox_open;
-    })
+    });
+}
+
+async function getDictionary(search_text) {
+    const api_url = dictionary_api + search_text;
+    try {
+        const response = await fetch(api_url);
+        const data = await response.json();
+
+        const word = data[0].word;
+        const means = data[0].meanings[0].definitions[0].definition;
+        const phonetics_part = data[0].phonetics
+        let word_audio = "";
+
+        for(let i=0; i<phonetics_part.length; i++){
+            if(phonetics_part[i].audio != ""){
+                word_audio = phonetics_part[i].audio;
+                break;
+            }
+        }
+
+        output_text = "Word : " + word + "; \n" + "Defination: " + means;
+        console.log(output_text);
+        console.log(word_audio)
+        promptGen(output_text, word_audio);
+        
+    } catch (error) {
+        console.log('Error', error);
+    }
 }
 
 
@@ -111,7 +171,7 @@ function doButton(){
         }
         textBox_open = !textBox_open;
         if(textBox_open){
-            promptGen();
+            getDictionary(prompt_text);
         }else{
             removeTextPrompt();
         }
